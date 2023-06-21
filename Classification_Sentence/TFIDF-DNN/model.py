a359241c61f7696a590ad411f5bdeff9c5c42218
hamy2078 <hamy2078@gmail.com>
2023-06-21 16:59:28 +0900

    update Classification_Sentence

--- Classification_Sentence/TFIDF-DNN/model.py
+++ Classification_Sentence/TFIDF-DNN/model.py
@@ -5,8 +5,9 @@ class DNN(nn.Module):
     def __init__(self):
         super().__init__()
         self.fc1 = nn.Linear(15000,10000)
-        self.fc2 = nn.Linear(10000,128)
-        self.fc3 = nn.Linear(128,1)
+        self.fc2 = nn.Linear(10000,512)
+        self.fc3 = nn.Linear(512,128)
+        self.fc4 = nn.Linear(128,1)
         
         self.dropout5 = nn.Dropout(0.5)
         self.dropout2 = nn.Dropout(0.2)
@@ -15,9 +16,17 @@ class DNN(nn.Module):
         self.sigmoid = nn.Sigmoid()
     def forward(self,x):
         x = self.fc1(x)
+        x = self.relu(x)
         x = self.dropout5(x)
+        
         x = self.fc2(x)
-        x = self.dropout2(x)
+        x = self.relu(x)
+        x = self.dropout5(x)
+        
         x = self.fc3(x)
+        x = self.relu(x)
+        x = self.dropout2(x)
+        
+        x = self.fc4(x)
         x = self.sigmoid(x)
         return x         
